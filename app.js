2667f7672ba187acbedfc676670ddd665245def6
AiltonRafael <ailton.rafael@zaig.com.br>
2021-11-30 12:49:10 -0300

    updated

--- app.js
+++ app.js
@@ -7,6 +7,25 @@ const allPokemon = require("./data");
 
 const app = express();
 
-// -- Define your route listeners here! --
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
 
+app.get('/pokemon', (req, res) => {
+    res.status(200).send(allPokemon)
+})
+
+app.get('/pokemon/:id', (req, res) => {
+    const pokemonMatchById = allPokemon.find((pokemonId) => (pokemonId.id) === parseInt(req.params.id))
+    res.status(200).send(pokemonMatchById)
+})
+
+app.get('/search/:name', (req, res) => {
+
+    // const pokemonMatchByName = allPokemon.find((currentPokemon) => (currentPokemon.name) === req.params.name)
+    // res.status(200).send(pokemonMatchByName)
+   
+    let pokemonMatchByTypes = allPokemon.filter((currentPokemon) => currentPokemon.types.includes((currentTypes) => console.log(currentTypes) === req.params.name))
+    res.status(200).send(`Hello dear ${pokemonMatchByTypes}`)
+   
+})
 app.listen(PORT, () => console.log(`Server up and running at port ${PORT}`));
